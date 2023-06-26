351d80750736c198564b7d022701ee1690afa79d
Ravi <dev.raviraghav93@gmail.com>
2023-06-26 23:46:55 +0530

    Initial commit

---
+++ Controllers/AllowAllController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AllowAllController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Working okay";
+        }
+    }
+}
